3de951d7a603d55c335a677c2da5e858ec527813
HamletDRC <HamletDRC@e38d0fe0-9136-0410-9dda-19804d5158af>
2007-08-02 19:24:38 +0000

    Defect#14 - Fixed F1 so that local help screen opens

--- trunk/src/com/timeTool/actions/HelpAction.java
+++ trunk/src/com/timeTool/actions/HelpAction.java
@@ -1,25 +1,26 @@
 package com.timeTool.actions;
 
+import com.timeTool.BrowserLaunch;
+
 import java.awt.event.ActionEvent;
 import java.io.File;
-import javax.swing.AbstractAction;
 
-import com.timeTool.BrowserLaunch;
+import javax.swing.AbstractAction;
 
 public class HelpAction extends AbstractAction
 {
     /**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -5780282110553360589L;
-	public static final String helpAction     = "help";    
+	public static final String helpAction     = "help";
 
-	public HelpAction() 
+	public HelpAction()
 	{
 	    super(helpAction);
 	}
-    public void actionPerformed(ActionEvent e) 
+    public void actionPerformed(ActionEvent e)
     {
-    	BrowserLaunch.openURL("docs" + File.separator + "Help.htm"); 
+    	BrowserLaunch.openURL("docs" + File.separator + "index.htm"); 
 	}
 }
